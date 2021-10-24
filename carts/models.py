55e02459758569f7d83318fb8dc13e2440c60ad8
anhquocphan99001 <quocanhtd12344@gmail.com>
2021-10-24 23:43:58 +0700

    complete by anh

--- carts/models.py
+++ carts/models.py
@@ -14,10 +14,17 @@ class Cart(models.Model):
 
 
 class CartItem(models.Model):
+    user = models.ForeignKey(Account, on_delete=models.CASCADE, null=True)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     quantity = models.IntegerField()
     is_active = models.BooleanField(default=True)
 
-    def __str__(self):
+    def sub_total(self):
+        return self.quantity * self.product.price
+
+    def __unicode__(self):
         return self.product
+
+    def __str__(self):
+        return self.product.__str__()
