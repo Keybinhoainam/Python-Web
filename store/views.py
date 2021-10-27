98a0c4764778f0b2713d85e5f420ec7dbdaeb28e
nam <keybinhoainam@gmail.com>
2021-10-27 17:32:46 +0700

    change

--- store/views.py
+++ store/views.py
@@ -64,5 +64,26 @@ def search(request):
         'product_count': product_count
     }
     return render(request, 'store/store.html', context=context)
-
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.method == "POST":
+        try:
+            review = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=review)
+            form.save()
+            messages.success(request, "Thank you! Your review has been updated.")
+            return redirect(url)
+        except Exception:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
+                messages.success(request, "Thank you! Your review has been submitted.")
+                return redirect(url)
 
