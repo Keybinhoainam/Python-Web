b34f2f5de4d9b723710674b890e33dd127abf539
nam <keybinhoainam@gmail.com>
2021-11-03 18:48:24 +0700

    done by nam

--- orders/views.py
+++ orders/views.py
@@ -92,6 +92,7 @@ def place_order(request, total=0, quantity=0,):
                 'categories': categories,
                 # 'tongsldaban':tong,
             }
+            cart_items.delete()
             return render(request, 'orders/payments.html', context)
     else:
         return redirect('checkout')
